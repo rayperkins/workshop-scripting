7a9c8c61a47281c5f06147af304c3159d75ef5c7
Ray Perkins <ray.perkins@weareglobal.co.nz>
2025-03-07 13:58:16 +1300

    modify example

--- Program.cs
+++ Program.cs
@@ -8,13 +8,11 @@
 var code = @"
 $i = 1 + 2
 call Print ""number = "" + $i
-call Print ""The End!"" 
 ".Trim();
 
 // Lexical analysis on the source code:
 
 // the below are some variables to track particular parts of things
-var inQuotes = false;
 var tokens = new List<string>();
 var currentTokenBuilder = new StringBuilder();
 
@@ -39,17 +37,6 @@ for (var ptr = 0; ptr < code.Length; ptr++)
 
     switch (ch, next)
     {
-        case ('"', _) when !inQuotes: // start tracking quote
-            currentTokenBuilder.Append('"');
-            inQuotes = true;
-            break;
-        case ('"', _) when inQuotes: // stop tracking quote
-            currentTokenBuilder.Append('"');
-            inQuotes = false;
-            break;
-        case (_, _ ) when inQuotes: // capture in quotes as a single token
-            currentTokenBuilder.Append(ch);
-            break;
         case ('\r', _): // ignore return char
         case ('\n', '\n'): // ignore consecutive lines
         case (' ', ' '): // ignore consecutive space
@@ -75,84 +62,3 @@ for(var i = 0; i < tokens.Count; i++)
 {
     Console.WriteLine($"{i}: {tokens[i]}");
 }
-
-/// Execute the tokens, this is a state machine that walks it's way
-/// through the list of tokens and decides what to do for each.
-var functionTarget = default(string);
-var variableTarget = default(string);
-var variableMerge = default(object);
-var variables = new Dictionary<string, object>();
-
-// handle extracting values from tokens or variables
-object GetTokenValue(string token)
-{
-    var value = default(object);
-    if(token[0] == '$') // is var
-    {
-        value = variables.GetValueOrDefault(token, null);
-    }
-    else if(token[0] == '"') // is string
-    {
-        value = token.Trim('"');
-    }
-    else if(double.TryParse(token, out var number)) // is number
-    {
-        value = number;
-    }   
-
-    return value;
-}
-
-for (var ptr = 0; ptr < tokens.Count; ptr++)
-{
-    var prev = ptr - 1 >= 0 ? tokens[ptr - 1] : null;
-    var current = tokens[ptr];
-    var next = ptr + 1 < tokens.Count ? tokens[ptr + 1] : null;
-
-    switch (prev, current, next)
-    {
-        // newline indicates end of a statement
-        case (_, "\n", _) when variableTarget != null:
-            variables[variableTarget] = variableMerge;
-            variableTarget = null;
-            break;
-        case (_, "\n", _) when functionTarget != null: // run function
-            if(functionTarget == "Print")
-            {
-                Console.WriteLine(variableMerge);
-            }
-            functionTarget = null;
-            break;
-        case (_, "+", _): // assume the left side is already the value of variableMerge
-            var nextValue = GetTokenValue(next);
-            variableMerge = (variableMerge, nextValue) switch
-            {
-                (null, null) => null,
-                (_, null) => variableMerge,
-                (null, _) => nextValue,
-                (double, string) => variableMerge.ToString() + nextValue,
-                (string, double) => variableMerge + nextValue.ToString(),
-                (string, string) => (string)variableMerge + (string)nextValue,
-                (double, double) => (double)variableMerge + (double)nextValue,
-            };
-            ptr ++;
-            break;
-        case (_, "=", _): // assign something, assume the prev is the target and next is merge
-            if(prev[0] == '$')
-            {
-                variableTarget = prev;
-                variableMerge = GetTokenValue(next);;
-                ptr ++;
-            }
-            break;
-        case ("call", _, _): // call function
-            functionTarget = current;
-            variableMerge = GetTokenValue(next);
-            ptr ++;
-            break;
-        case (_, _, _) when variableTarget != null: 
-            break;
-    }
-}
-
-Console.WriteLine("");
